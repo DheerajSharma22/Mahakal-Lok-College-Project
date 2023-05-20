0fc172968036040b35f7859509c9c14b4debc6ae
Dheeraj Sharma <dsprofessional12@gmail.com>
2023-05-20 11:24:00 +0530

    Home Page is Completed

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 // Code For Setting Background Color to Active Links.
 const windowLoc = window.location.pathname;
 const navLinks = document.querySelectorAll("#menu li a");
+const navbar = document.querySelector(".navbar");
 
 if (window.location.pathname == "/") {
   navLinks[0].classList.add("active");
@@ -12,3 +13,11 @@ if (window.location.pathname == "/") {
   });
 }
 
+window.onscroll = () => {
+  if (window.pageYOffset == 0) {
+    navbar.classList.remove("navbarOnScroll");
+  }
+  if (window.pageYOffset > 0) {
+    navbar.classList.add("navbarOnScroll");
+  }
+};
